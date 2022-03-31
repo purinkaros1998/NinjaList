a8c4300159988112bb6f0775f60788d019ca4757
Purin Sila <purin.s@sabuytech.com>
2022-03-31 16:36:58 +0700

    inital

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur quidem placeat excepturi, repellat accusamus maxime, pariatur a cumque magnam ipsam perspiciatis veniam impedit ab fugit deserunt debitis sit mollitia nesciunt!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur quidem placeat excepturi, repellat accusamus maxime, pariatur a cumque magnam ipsam perspiciatis veniam impedit ab fugit deserunt debitis sit mollitia nesciunt!</p>
+            </div>
+        </>
+
+    );
+}
+
+export default About;
